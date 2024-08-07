d1f7ef92d95983df3b01199e40b2976c2264027e
Santiago-pe <penasantiago346@gmail.com>
2024-08-06 21:05:25 -0300

    add bucket

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3"{
-    bucket                 = "mundose22"
+    bucket                 = "mundose-tf-2024"
     region                 = "us-east-1"
-    key                    = "backend.tfstate"
-    dynamodb_table         = "terraformstatelock"
+    key                    = "ec2/backend.tfstate"
+    # dynamodb_table         = "terraformstatelock"
   }
 }
 
