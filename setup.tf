85fd8113f5dcffd6b15535863de12127c032864b
Santiago-pe <penasantiago346@gmail.com>
2024-08-06 21:13:06 -0300

    add resource

--- setup.tf
+++ setup.tf
@@ -91,3 +91,53 @@ provider "aws" {
 # output "Webserver-Public-IP" {
 #   value = aws_instance.webserver.public_ip
 # }
+
+data "aws_vpc" "vpc_default" {
+
+  default = true
+
+}
+
+#data source para obtener la region
+
+data "aws_region" "current" {}
+
+ 
+
+#data source para id de la cuenta de AWS
+
+data "aws_caller_identity" "current" {}
+
+data "aws_subnet" "az_a" {
+
+  vpc_id            = data.aws_vpc.vpc_default.id
+
+  availability_zone = "us-east-1a"
+
+}
+
+resource "aws_security_group" "sg" {
+  name        = "sg"
+  description = "Allow TCP/80 & TCP/22"
+  vpc_id      = data.aws_vpc.vpc_default.id
+  ingress {
+    description = "Allow SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "allow traffic from TCP/80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
